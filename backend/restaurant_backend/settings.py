7c00b4a1f4e0c1286cf85315fb1356854bd0f197
Nived <nivedsuresh5@gmail.com>
2026-03-21 13:51:18 +0530

    requirments.txt - gunicorn added and settings.py frontend url added

--- backend/restaurant_backend/settings.py
+++ backend/restaurant_backend/settings.py
@@ -160,7 +160,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173 ",
     "https://foo-d-elivery.netlify.app",
     "http://thecrunchindia.com",
-    "https://frontend-production-0d8cc.up.railway.app/",
+    "https://frontend-production-0d8cc.up.railway.app",
 
 ]
 
